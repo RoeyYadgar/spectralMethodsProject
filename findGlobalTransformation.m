ee299204e4e4484637e09e7e937f7306c7a1d0c8
a <a@a.com>
2023-02-13 10:49:10 +0200

    added global transformation function

---
+++ findGlobalTransformation.m
@@ -0,0 +1,27 @@
+function [reflections,rotations] = findGlobalTransformation(patchReflection,patchRotation,Adj)
+
+N = length(patchReflection);
+z = patchReflection.*(Adj~=3);
+connectedPatches = find(conncomp(graph(abs(z))) == 1);
+z = z(connectedPatches,connectedPatches);
+Z = sparse(diag(sum(abs(z),2).^(-1)))*z;
+[Vref,~] = eigs(Z,1);
+reflections = zeros(N,1);
+reflections(connectedPatches) = sign(Vref);
+plot(abs(Vref));
+
+r = triu(patchRotation);
+for i = 1:N
+    if(reflections(i) == -1)
+        r(i,:) = conj(r(i,:));
+    end
+end
+r = r(connectedPatches,connectedPatches);
+r = r+r';
+R = sparse(diag(sum(abs(r),2).^(-1)))*r;
+[Vrot,~] = eigs(R,1);
+rotations = zeros(size(reflections));
+rotations(connectedPatches) = Vrot(:,1)./abs(Vrot(:,1));
+
+
+x = 1;
