bc3d11d19f9959799e9966ae73380353fc763578
Yudugur <91159372+Yudugur@users.noreply.github.com>
2023-03-07 13:41:13 +0200

    updated comments to plotPatch.m

--- plotPatch.m
+++ plotPatch.m
@@ -1,9 +1,15 @@
 function plotPatch(patch,pos)
+%plots a patch
+%inputs:
+%   patch - patch graph 
+%   pos - position of nodes of the patch
 
-
+%if pos was not given as an input, use the Pos attribute of the graph
 if(nargin == 1)
    pos = patch.Nodes.Pos; 
 end
+
+%if pos is Nx2 or Nx3 matrix use each column as the cooridnates of x,y,z
 if(size(pos,2) >= 2)
     xPos = pos(:,1);
     yPos = pos(:,2);
@@ -13,6 +19,7 @@ if(size(pos,2) >= 2)
         zPos = zeros(size(xPos));
     end
 end
+%if pos is complex use the real and imaginary parts as the cooridnates of x,y
 if(~isreal(pos))
     xPos = real(pos);
     yPos = imag(pos);
@@ -20,7 +27,7 @@ if(~isreal(pos))
 
 end
 
-    
+%plot the graph
 p = plot(patch,'XData',xPos,'YData',yPos,'ZData',zPos);
 p.LineWidth = 2;
 view(0,90)
